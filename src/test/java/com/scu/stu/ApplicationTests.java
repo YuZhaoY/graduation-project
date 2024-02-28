91d359045b639c0ae71b861725029ec021aba74e
yzy <3044059772@qq.com>
2024-02-28 21:22:56 +0800

    Initial commit

---
+++ src/test/java/com/scu/stu/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scu.stu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
